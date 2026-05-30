25a2ff531b665e754bd4d2dc9fed859c6efe06d8
Ana Cristina Chavez Acosta <a01742237@tec.mx>
2026-05-29 18:05:34 -0600

    Add DATA_MEMORY module with memory initialization

---
+++ RISC-V/DATA_MEMORY.v
@@ -0,0 +1,28 @@
+//ejemplo la memoria del periodo pasado
+//Sacar del ejemplo
+//Crearle el archivo para iniciar con valores random simples
+
+module DATA_MEMORY (
+	input clk,
+	input WE,
+	input [31:0] A,
+	input [31:0] WD,
+	output [31:0] RD
+);
+
+reg [31:0] mem [0:63];
+
+initial begin
+	$readmemh("mem.hex", mem); //Archivo en carpeta se llama mem.hex 
+end
+
+
+assign RD = mem[A[31:2]];
+
+always @(posedge clk)
+begin
+	if (WE)
+		mem[A[31:2]] <= WD;
+end
+
+endmodule
