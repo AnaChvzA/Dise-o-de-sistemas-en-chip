71e5b8cc2044f208bc5f8e7f3f3dc9699584e178
Ana Cristina Chavez Acosta <a01742237@tec.mx>
2026-05-29 18:01:16 -0600

    Rename module from INSTRUCTION_MEMORY to instruction_memory

---
+++ RISC-V/instruction_memory.v
@@ -0,0 +1,18 @@
+module instruction_memory (
+	input [31:0] A,
+	output [31:0] RD
+);
+
+reg [31:0] instr_mem [0:63];
+
+initial begin
+	$readmemh("instrMem.hex", instr_mem);
+	$display("instr_mem[0] = %h", instr_mem[0]);
+	$display("instr_mem[1] = %h", instr_mem[1]);
+	$display("instr_mem[2] = %h", instr_mem[2]);
+end
+
+//No me funciona con secuencial asi que es combinacional 
+assign RD = instr_mem[A[31:2]];
+
+endmodule
